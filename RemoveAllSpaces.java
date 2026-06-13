9b36c5514d575bedbc4c6731352e2a5f91758af6
milanesh28 <milanesh1234@gmail.com>
2026-06-13 17:11:37 +0530

    Create  RemoveAllSpaces.java

---
+++ RemoveAllSpaces.java
@@ -0,0 +1,12 @@
+package placement;
+
+public class RemoveAllSpaces {
+
+	public static void main(String[] args) {
+		String str = "Hello World Java";
+		String result = str.replace(" ", "");
+		System.out.println(result); 
+
+	}
+
+}
