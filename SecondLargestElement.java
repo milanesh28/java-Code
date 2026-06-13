edcea62100b6a93dfe2265a0ed161f37fc55cdd9
milanesh28 <milanesh1234@gmail.com>
2026-06-13 17:18:00 +0530

    Create  SecondLargestElement.java

---
+++ SecondLargestElement.java
@@ -0,0 +1,27 @@
+package placement;
+
+public class SecondLargestElement {
+    public static int findSecondLargest(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            throw new IllegalArgumentException("Array must have at least 2 elements");
+        }
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        if (secondLargest == Integer.MIN_VALUE) {
+            throw new RuntimeException("No second largest element (all elements may be equal)");
+        }
+        return secondLargest;
+    }
+     public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 8, 20};
+        System.out.println("Second largest: " + findSecondLargest(arr));
+    }
+}
