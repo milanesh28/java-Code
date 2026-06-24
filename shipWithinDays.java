ed86a5d3055f5df10172f8eb9ced7f970ada3dd9
milanesh28 <milanesh1234@gmail.com>
2026-06-24 10:32:33 +0530

    Create shipWithinDays.java

---
+++ shipWithinDays.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int low = 0, high = 0;
+
+        for (int w : weights) {
+            low = Math.max(low, w);
+            high += w;
+        }
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (canShip(weights, days, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private boolean canShip(int[] weights, int days, int capacity) {
+        int currentLoad = 0;
+        int requiredDays = 1;
+
+        for (int w : weights) {
+            if (currentLoad + w > capacity) {
+                requiredDays++;
+                currentLoad = 0;
+            }
+
+            currentLoad += w;
+        }
+
+        return requiredDays <= days;
+    }
+}
